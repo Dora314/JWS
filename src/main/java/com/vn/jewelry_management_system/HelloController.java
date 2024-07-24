4179042b46ecdab97a340910f36a5452ec2a16e6
1anhphanle <anhphanle8818@gmail.com>
2024-07-24 21:41:51 +0700

    test port 8080

---
+++ src/main/java/com/vn/jewelry_management_system/HelloController.java
@@ -0,0 +1,12 @@
+package com.vn.jewelry_management_system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Spring Boot!";
+    }
+}
