ae1b72c7996f6f68e778ffe7514f4b39ecb2b1d1
1anhphanle <anhphanle8818@gmail.com>
2024-08-02 21:24:14 +0700

    submit user creation form

--- src/main/java/com/vn/jewelry_management_system/controller/UserController.java
+++ src/main/java/com/vn/jewelry_management_system/controller/UserController.java
@@ -2,8 +2,11 @@ package com.vn.jewelry_management_system.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.vn.jewelry_management_system.domain.User;
 import com.vn.jewelry_management_system.service.UserService;
 
 @Controller
@@ -17,18 +20,19 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("test", test);
-        model.addAttribute("anh", "from controller with model");
         return "hello";
     }
 
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
-        model.addAttribute("hoidanit", "from controller with model");
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User anh) {
+        System.out.println(" run here " + anh);
+        return "hello";
     }
 
 }
