de6e125cfb55aae2ef1771c393a5466ffa32d655
Dora314 <thienduy1001@gmail.com>
2024-08-21 10:21:00 +0700

    remove all unused imports and add nonnull annotation, also add database config file to gitignore

--- src/main/java/com/vn/jewelry_management_system/repository/StallRepository.java
+++ src/main/java/com/vn/jewelry_management_system/repository/StallRepository.java
@@ -3,6 +3,7 @@ package com.vn.jewelry_management_system.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.vn.jewelry_management_system.domain.Stall;
+import org.springframework.lang.NonNull;
 import java.util.Optional;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public interface StallRepository extends JpaRepository<Stall, Integer> {
     Optional<Stall> findByStallName(String stallName);
 
+    @NonNull
     List<Stall> findAll();
 
 }
